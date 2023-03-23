d991b656d0177050224388d8b79e018a6a51295a
percy <percymichael0@gmail.com>
2023-03-23 09:37:30 +0300

    trying the web-fluid

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,60 +1,11 @@
-import { OrbitControls, Stage, useVideoTexture } from "@react-three/drei";
-import { Canvas, useLoader } from "@react-three/fiber";
-import { BillBoard } from "../components/BillBoard";
-import { TextureLoader } from "three";
-
-function Circle() {
-  return (
-    <mesh position={[0, 0.5, 0]}>
-      <sphereGeometry args={[0.5, 100]} />
-
-      <meshStandardMaterial metalness={1} roughness={0} />
-    </mesh>
-  );
-}
-
-function Tv() {
-  const video = useVideoTexture("/20ths.mp4", { muted: 1 });
-  return (
-    <mesh position={[3, 1.86, 0.1]}>
-      <planeGeometry args={[2.05, 0.877]} />
-
-      <meshStandardMaterial map={video} />
-    </mesh>
-  );
-}
-
-function Cube() {
-  const colorMap = useLoader(TextureLoader, "wood.jpeg");
-  return (
-    <mesh position={[6, 0.2, 0.1]}>
-      <boxGeometry args={[0.5, 0.5]} />
-
-      <meshStandardMaterial map={colorMap} />
-    </mesh>
-  );
-}
+import React from "react";
+import WebGLFluid from "webgl-fluid";
 
 const index = () => {
-  return (
-    <div className="h-screen">
-      <Canvas camera={{ position: [0, 3, 20] }}>
-        <OrbitControls
-          maxPolarAngle={Math.PI / 1.8}
-          minPolarAngle={Math.PI / 6.5}
-          minDistance={5}
-          maxDistance={10}
-        />
-
-        <Stage>
-          <Circle />
-          <BillBoard position={[3, 0, 0]} scale={0.01} />
-          <Cube />
-          <Tv />
-        </Stage>
-      </Canvas>
-    </div>
-  );
+  WebGLFluid(document.querySelector("canvas"), {
+    // options
+  });
+  return <canvas></canvas>;
 };
 
 export default index;
